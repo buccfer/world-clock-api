71fc3870a152c73b65a935819e6c99f4d60ce407
Fernando Buccella <fer.buccella@gmail.com>
2020-12-15 09:56:23 -0300

    Add request logger.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 'use strict'
 
 const express = require('express')
+const morgan = require('morgan')
+const { REQUEST_LOGGER_FORMAT } = require('./config')
 
 const app = express()
+
+app.use(morgan(REQUEST_LOGGER_FORMAT))
 
 module.exports = app
--- src/config.js
+++ src/config.js
@@ -1,13 +1,18 @@
 'use strict'
 
 const defaultTo = require('lodash/defaultTo')
+const parseInt = require('lodash/parseInt')
 
 const {
+  NODE_ENV,
   PORT,
-  LOG_LEVEL
+  LOG_LEVEL,
+  REQUEST_LOGGER_FORMAT
 } = process.env
 
 module.exports = {
-  PORT: defaultTo(PORT, 3000),
-  LOG_LEVEL: defaultTo(LOG_LEVEL, 'info')
+  NODE_ENV: defaultTo(NODE_ENV, 'development'),
+  PORT: defaultTo(parseInt(PORT), 3000),
+  LOG_LEVEL: defaultTo(LOG_LEVEL, 'info'),
+  REQUEST_LOGGER_FORMAT: defaultTo(REQUEST_LOGGER_FORMAT, 'dev')
 }
