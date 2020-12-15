91f8b15ab641052a44b53a522e761dc29de55a50
Fernando Buccella <fer.buccella@gmail.com>
2020-12-15 09:59:01 -0300

    Add helmet to enhance security.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@
 
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
 const { REQUEST_LOGGER_FORMAT } = require('./config')
 
 const app = express()
 
 app.use(morgan(REQUEST_LOGGER_FORMAT))
+app.use(helmet())
 
 module.exports = app
